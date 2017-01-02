6124b92a3525348ac84d0645c4c8ebd200ddf376
EQuimper <quimperemanuel@gmail.com>
2017-01-01 23:28:06 -0500

    Handle the actions for liked

--- Routes.js
+++ Routes.js
@@ -8,13 +8,79 @@ import {
   HomeContainer,
   GameStreamsContainer,
   GamesFollowScreen,
-  LiveStreamContainer,
+  LiveStreamScreen,
   toggleSearchGames,
   resetGameStreams,
   searchGamesInput,
-  GameResultsContainer
+  GameResultsContainer,
+  toggleLiked
 } from './modules';
 import { SearchBar } from './components';
+
+const Routes = ({ toggleSearchGames, resetGameStreams, searchGamesInput, toggleLiked }) => (
+  <Router
+    sceneStyle={styles.sceneStyle}
+    navigationBarStyle={styles.navBar}
+    titleStyle={styles.titleStyle}
+    leftButtonIconStyle={styles.leftButton}
+  >
+    <Scene
+      key="home"
+      initial
+      titleProps={{ navBarShow: true }}
+      title="HelloWorld"
+      renderTitle={({ navBarShow }) =>
+        <SearchBar navBarShow={navBarShow} searchGamesInput={searchGamesInput} />
+      }
+      component={HomeContainer}
+      renderRightButton={({ navBarShow }) => (
+        <TouchableOpacity onPress={() => toggleSearchGames()}>
+          <View style={styles.rightButton}>
+            {navBarShow ? (
+              <Text style={[styles.titleStyle, styles.cancelText]}>Cancel</Text>
+            ) : (
+              <FontAwesome name="search" color="white" size={20} />
+            )}
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+    <Scene
+      onBack={() => resetGameStreams()}
+      key="gameStreams"
+      renderRightButton={props => (
+        <TouchableOpacity onPress={() => toggleLiked(props.game, props.image)}>
+          <View style={styles.rightButton}>
+            <FontAwesome
+              name={props.liked}
+              color="white" size={30}
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+      component={GameStreamsContainer}
+    />
+    <Scene
+      key="liveStream"
+      renderRightButton={props => (
+        <View style={styles.rightButton}>
+          <FontAwesome name={props.liked ? 'heart' : 'heart-o'} color="white" size={30} />
+        </View>
+      )}
+      component={LiveStreamScreen}
+    />
+    <Scene
+      key="resultsGame"
+      title="Results"
+      component={GameResultsContainer}
+    />
+    <Scene
+      key="gamesFollow"
+      title="Games Followed"
+      component={GamesFollowScreen}
+    />
+  </Router>
+);
 
 const styles = EStyleSheet.create({
   navBar: {
@@ -40,67 +106,9 @@ const styles = EStyleSheet.create({
   }
 });
 
-const Routes = ({ toggleSearchGames, resetGameStreams, searchGamesInput }) => (
-  <Router
-    sceneStyle={styles.sceneStyle}
-    navigationBarStyle={styles.navBar}
-    titleStyle={styles.titleStyle}
-    leftButtonIconStyle={styles.leftButton}
-  >
-    <Scene
-      key="home"
-      initial
-      titleProps={{ navBarShow: true }}
-      title="HelloWorld"
-      renderTitle={({ navBarShow }) => <SearchBar navBarShow={navBarShow} searchGamesInput={searchGamesInput} />}
-      component={HomeContainer}
-      renderRightButton={({ navBarShow }) => (
-        <TouchableOpacity onPress={() => toggleSearchGames()}>
-          <View style={styles.rightButton}>
-            {navBarShow ? (
-              <Text style={[styles.titleStyle, styles.cancelText]}>Cancel</Text>
-            ) : (
-            <FontAwesome name="search" color="#333333" size={25} />
-            )}
-          </View>
-        </TouchableOpacity>
-      )}
-    />
-    <Scene
-      onBack={() => resetGameStreams()}
-      key="gameStreams"
-      renderRightButton={props => (
-        <View style={styles.rightButton}>
-          <FontAwesome name={props.liked ? 'heart' : 'heart-o'} color="white" size={30} />
-        </View>
-      )}
-      component={GameStreamsContainer}
-    />
-    <Scene
-      key="liveStream"
-      renderRightButton={props => (
-        <View style={styles.rightButton}>
-          <FontAwesome name={props.liked ? 'heart' : 'heart-o'} color="white" size={30} />
-        </View>
-      )}
-      component={LiveStreamContainer}
-    />
-    <Scene
-      key="resultsGame"
-      title="Results"
-      component={GameResultsContainer}
-    />
-    <Scene
-      key="gamesFollow"
-      title="Games Followed"
-      component={GamesFollowScreen}
-    />
-  </Router>
-);
-
 export default connect(
   state => ({
     navBar: state.ui.navBar
   }),
-  { toggleSearchGames, resetGameStreams, searchGamesInput }
+  { toggleSearchGames, resetGameStreams, searchGamesInput, toggleLiked }
 )(Routes);
