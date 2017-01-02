53333fd34d2b9715e679a38034fa5f5fc8d34b6f
EQuimper <quimperemanuel@gmail.com>
2017-01-01 23:27:39 -0500

    Actions for fetch offset streams

--- modules/gameStreams/actions.js
+++ modules/gameStreams/actions.js
@@ -1,16 +1,21 @@
 import { Actions } from 'react-native-router-flux';
 import Reactotron from 'reactotron-react-native';
-import { getGameStreams } from '../../helpers/api';
+import { getGameStreams, getGameStreamsOffset } from '../../helpers/api';
 
 export const FETCH_GAME_STREAMS = 'FETCH_GAME_STREAMS';
 export const RESET_GAME_STREAMS = 'RESET_GAME_STREAMS';
+export const FETCH_OFFSET_STREAMS = 'FETCH_OFFSET_STREAMS';
 
 export const resetGameStreams = () => {
   Actions.pop({ type: 'reset' });
   return { type: RESET_GAME_STREAMS };
 };
 
-export const fetchGameStreams = game => {
+export const fetchGameStreams = (game, reset) => dispatch => {
+  if (reset) {
+    dispatch({ type: RESET_GAME_STREAMS });
+  }
+
   const promise = new Promise((resolve, reject) => {
     return getGameStreams(game)
       .then(
@@ -19,8 +24,25 @@ export const fetchGameStreams = game => {
       );
   });
 
-  return {
+  return dispatch({
     type: FETCH_GAME_STREAMS,
+    payload: promise
+  });
+};
+
+export const paginateStreams = (game, offset) => {
+  Reactotron.log('HELLLO');
+  Reactotron.log({ game, offset });
+  const promise = new Promise((resolve, reject) => {
+    return getGameStreamsOffset(game, offset)
+      .then(
+        res => resolve(res.streams),
+        err => reject(err)
+      );
+  });
+
+  return {
+    type: FETCH_OFFSET_STREAMS,
     payload: promise
   };
 };
